3bfdbf46ab494bd6aff56b724d4ec7a222918513
T0mt4ru <Tomtaru@gmail.com>
2026-07-31 18:11:22 +0200

    Finished Crusher recipes, 28 recipes created.

--- src/main/java/com/tomtaru/tmtceic/datagen/recipeproviders/SqueezerRecipes.java
+++ src/main/java/com/tomtaru/tmtceic/datagen/recipeproviders/SqueezerRecipes.java
@@ -13,16 +13,17 @@ import net.neoforged.neoforge.fluids.FluidStack;
 public class SqueezerRecipes {
 
     private static final int energyStandard = 3200;
-    private static final int fluidAmountQuarterBucket = 250;
+    private static final int fluidAmountEighthBucket = 125;
 
     public static void build(RecipeOutput output) {
 
         for (String aprigel : Dexes.APRIDEX) {
+
             ResourceLocation apricornID = ResourceLocation.fromNamespaceAndPath("cobblemon", aprigel + "_apricorn");
             IngredientWithSize apricornItem = new IngredientWithSize(Ingredient.of(BuiltInRegistries.ITEM.get(apricornID)), 1);
 
             ResourceLocation aprigelID = ResourceLocation.fromNamespaceAndPath(Tmtceic.MODID, aprigel + "_aprigel");
-            FluidStack aprigelFluid = new FluidStack(BuiltInRegistries.FLUID.get(aprigelID), fluidAmountQuarterBucket);
+            FluidStack aprigelFluid = new FluidStack(BuiltInRegistries.FLUID.get(aprigelID), fluidAmountEighthBucket);
 
             ResourceLocation dyeID = ResourceLocation.fromNamespaceAndPath("minecraft", aprigel +"_dye");
             TagOutput dyeItem = new TagOutput(BuiltInRegistries.ITEM.get(dyeID));
@@ -30,6 +31,19 @@ public class SqueezerRecipes {
             String recipeName = aprigel + "_aprigel";
 
             generateSqueezerRecipe(output, apricornItem, aprigelFluid, dyeItem, energyStandard, recipeName);
+        }
+
+        for (String mint : Dexes.MINTDEX) {
+
+            ResourceLocation seedID = ResourceLocation.fromNamespaceAndPath("cobblemon", mint + "_mint_seeds");
+            IngredientWithSize seedItem = new IngredientWithSize(Ingredient.of(BuiltInRegistries.ITEM.get(seedID)), 1);
+
+            ResourceLocation plantoilID = ResourceLocation.fromNamespaceAndPath("immersiveengineering", "plantoil");
+            FluidStack plantoilFluid = new FluidStack(BuiltInRegistries.FLUID.get(plantoilID), 80);
+
+            String recipeName = "plantoil_from_" +  mint + "_mint_seeds";
+
+            generateSqueezerRecipe(output, seedItem, plantoilFluid, TagOutput.EMPTY, energyStandard, recipeName);
         }
     }
 
