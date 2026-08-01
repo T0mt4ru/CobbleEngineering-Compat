252e4dd76e5822199f317f35534e7e0073511ec0
T0mt4ru <Tomtaru@gmail.com>
2026-08-01 12:19:27 +0200

    Added crafting recipe for custom blueprint. added description key for custom blueprint.

--- src/main/java/com/tomtaru/tmtceic/datagen/recipeproviders/BlueprintRecipes.java
+++ src/main/java/com/tomtaru/tmtceic/datagen/recipeproviders/BlueprintRecipes.java
@@ -4,14 +4,18 @@ import blusunrize.immersiveengineering.api.crafting.BlueprintCraftingRecipe;
 import blusunrize.immersiveengineering.api.crafting.IngredientWithSize;
 import blusunrize.immersiveengineering.api.crafting.TagOutput;
 import blusunrize.immersiveengineering.common.register.IEItems;
+import com.cobblemon.mod.common.CobblemonItems;
+import com.cobblemon.mod.common.item.CobblemonItem;
 import com.tomtaru.tmtceic.Tmtceic;
 import com.tomtaru.tmtceic.registry.ModItems;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BlueprintRecipes {
@@ -28,6 +32,28 @@ public class BlueprintRecipes {
                 "molds",
                 "mold_pokeball"
         );
+
+        for (String colour : Dexes.APRIDEX) {
+
+            ResourceLocation ballID = ResourceLocation.fromNamespaceAndPath("cobblemon", Dexes.APRICORN_TO_ANCIENTBALL.get(colour) + "_ball");
+            TagOutput ballItem = new TagOutput(BuiltInRegistries.ITEM.get(ballID));
+
+            ResourceLocation apricornID = ResourceLocation.fromNamespaceAndPath("cobblemon",  colour + "_apricorn");
+            List<IngredientWithSize> ingredients = List.of(
+                    new IngredientWithSize(Ingredient.of(BuiltInRegistries.ITEM.get(apricornID)), 2),
+                    new IngredientWithSize(Ingredient.of(CobblemonItems.TUMBLESTONE), 2),
+                    new IngredientWithSize(Ingredient.of(Items.STONE_BUTTON),1)
+            );
+
+            generateBlueprintRecipe(
+                    output,
+                    ballItem,
+                    ingredients,
+                    "ancient_pokeballs",
+                    Dexes.APRICORN_TO_ANCIENTBALL.get(colour) + "_ball"
+
+            );
+        }
     }
 
     private static void generateBlueprintRecipe(RecipeOutput blueprintOutput, TagOutput outputItem, List<IngredientWithSize> inputItems, String category, String recipeName) {
--- src/main/java/com/tomtaru/tmtceic/datagen/recipeproviders/VanillaRecipes.java
+++ src/main/java/com/tomtaru/tmtceic/datagen/recipeproviders/VanillaRecipes.java
@@ -1,25 +1,41 @@
 package com.tomtaru.tmtceic.datagen.recipeproviders;
 
 import blusunrize.immersiveengineering.api.EnumMetals;
+import blusunrize.immersiveengineering.api.crafting.BlueprintCraftingRecipe;
 import blusunrize.immersiveengineering.common.register.IEItems;
+import com.cobblemon.mod.common.CobblemonItems;
+import com.tomtaru.tmtceic.Tmtceic;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 
 public class VanillaRecipes {
+
     public static void build(RecipeOutput output) {
-        //ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.STEEL_KNIFE.get())
-        //        .pattern(" I ")
-        //        .pattern(" S ")
-        //        .define('I', IEItems.Metals.INGOTS.get(EnumMetals.STEEL))
-        //        .define('S', Items.STICK)
-        //        .unlockedBy("has_steel_ingot", InventoryChangeTrigger.TriggerInstance.hasItems(IEItems.Metals.INGOTS.get(EnumMetals.STEEL)))
-        //        .save(output);
-//
+
+        ItemStack ancientBallBlueprint = BlueprintCraftingRecipe.getTypedBlueprint("ancient_pokeballs");
+
+        TagKey<Item> apricornsTag = TagKey.create(Registries.ITEM,
+                ResourceLocation.fromNamespaceAndPath("cobblemon", "apricorns"));
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ancientBallBlueprint)
+                .pattern(" A ")
+                .pattern("DDD")
+                .pattern("PPP")
+                .define('A', Ingredient.of(apricornsTag))
+                .define('D', Items.BLUE_DYE)
+                .define('P', Items.PAPER)
+                .unlockedBy("has_apricorn", InventoryChangeTrigger.TriggerInstance.hasItems(CobblemonItems.RED_APRICORN))
+                .save(output, ResourceLocation.fromNamespaceAndPath(Tmtceic.MODID, "blueprint_item/ancient_pokeballs_blueprint"));
+
         //ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Items.SLIME_BALL)
         //        .requires(Items.LIME_DYE)
         //        .requires(vectorwing.farmersdelight.common.registry.ModItems.WHEAT_DOUGH.get())
